99b403cf9861cedc68efd43186b5beca11e62e70
Pastortz <albertotzita7@gmail.com>
2024-10-02 22:59:29 -0600

    Delete TourVirtual-React-main directory

--- TourVirtual-React-main/src/hooks/useModal.js
+++
@@ -1,8 +0,0 @@
-import {useState} from 'react';
-
-export const UseModal = (initialValue = false) => {
-    const [isOpen, setlsOpen] = useState(initialValue);
-    const openModal = ()=> {setlsOpen(true);};
-    const closeModal = ()=> {setlsOpen(false);};
-    return {isOpen, openModal, closeModal};
-};
